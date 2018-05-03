b1afdc7cf0b647572fb38813552c1e72a619ced5
Chorehu <287497281@qq.com>
2018-05-03 18:12:07 +0800

    commit2.5

---
+++ src/components/second5.vue
@@ -0,0 +1,60 @@
+<template>
+ <el-container>
+     <el-header></el-header>
+     <el-main>
+         <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+            <el-form-item prop="name">
+                <el-input v-model="ruleForm.name" placeholder="请输入文字" ></el-input>
+            </el-form-item>
+            <el-form-item prop="name">
+                <el-checkbox-group v-model="ruleForm.type">
+                    <el-checkbox name="type" label="1"></el-checkbox>
+                    <el-checkbox name="type" label="2"></el-checkbox>
+                    <el-checkbox name="type" label="3"></el-checkbox>
+                    <el-checkbox name="type" label="4"></el-checkbox>
+                    <el-checkbox name="type" label="5" disabled></el-checkbox>
+                    <el-checkbox name="type" label="6" disabled></el-checkbox>
+                </el-checkbox-group>
+            </el-form-item>
+         </el-form>
+     </el-main>
+ </el-container>
+</template>
+<style>
+.el-checkbox__inner:hover{
+    border-color: grey
+}
+
+.el-checkbox__input.is-checked+.el-checkbox__label{
+    color: red;
+}
+.el-checkbox__input.is-checked .el-checkbox__inner{
+    background-color: transparent;
+    border-color: red;
+}
+.el-checkbox__input.is-checked .el-checkbox__inner::after{
+    border-color: red;
+}
+</style>
+
+<script>
+export default{
+  data () {
+    return {
+      ruleForm: {
+        name: '',
+        type: ['3', '6']
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        type: [
+          { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+        ]
+      }
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,47 @@ import test3 from '@/components/test3'
 import second2 from '@/components/second2'
 import second3 from '@/components/second3'
 import second4 from '@/components/second4'
+import second5 from '@/components/second5'
+import VeeValidate, { Validator } from 'vee-validate'
+import zh from 'vee-validate/dist/locale/zh_CN'// 引入中文文件
 
+// 配置中文
+Validator.localize(zh)
+
+const config = {
+  locale: 'zh_CN',
+  fieldsBagName: 'formfields'
+}
+
+Vue.use(VeeValidate, config)
+
+// // 自定义validate
+// const dictionary = {
+//   zh_CN: {
+//     messages: {
+//       email: () => '请输入正确的邮箱格式',
+//       required: (field) => '请输入' + field
+//     },
+//     attributes: {
+//       email: '邮箱',
+//       password: '密码',
+//       name: '账号',
+//       phone: '手机'
+//     }
+//   }
+// }
+// console.log(Validator)
+// debugger
+// Validator.updateDictionary(dictionary)
+
+// Validator.extend('phone', {
+//   messages: {
+//     zh_CN: field => field + '必须是11位手机号码'
+//   },
+//   validate: value => {
+//     return value.length === 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
+//   }
+// })
 Vue.use(Router)
 Vue.use(ElementUI)
 
@@ -49,6 +89,11 @@ export default new Router({
       path: '/second4',
       name: 'second4',
       component: second4
+    },
+    {
+      path: '/second5',
+      name: 'second5',
+      component: second5
     }
   ]
 })
